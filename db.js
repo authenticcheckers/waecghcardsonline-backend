1cf86ae153b685b41ad378906c112cc450effe52
authenticcheckers <authenticcheckers@gmail.com>
2025-11-19 14:41:11 +0000

    Add files via upload

---
+++ db.js
@@ -0,0 +1,22 @@
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import fs from 'fs';
+import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const DB_FILE = process.env.DB_FILE || './data.db';
+
+async function migrate() {
+  const db = await open({ filename: DB_FILE, driver: sqlite3.Database });
+  const migrations = fs.readFileSync(path.join('./migrations.sql'), 'utf8');
+  await db.exec(migrations);
+  console.log('Migrations applied to', DB_FILE);
+  await db.close();
+}
+
+if (process.argv[2] === 'migrate') {
+  migrate();
+} else {
+  migrate();
+}
